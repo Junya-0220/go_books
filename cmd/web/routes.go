aae1bb728d8884f7aad7073ba8def45af2fa7d6f
Junya-0220 <junya010220@gmail.com>
2023-09-04 23:01:56 +0900

    Initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Junya-0220/go_books/pkg/config"
+	"github.com/Junya-0220/go_books/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
